9cd9d574550d882730384e7ced17a42f5791d454
1BM18CS147 <71380699+1BM18CS147@users.noreply.github.com>
2020-10-08 15:14:08 +0530

    Add files via upload

---
+++ lab03/Greatestof3.sh
@@ -0,0 +1,13 @@
+echo "Enter 3 numbers"
+read num1
+read num2
+read num3
+if [ $num1 -gt $num2 ] && [ $num1  -gt $num3 ]
+then
+echo "$num1 is the greatest"
+elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
+then
+echo "$num2 is the greatest"
+else
+echo "$num3 the is greatest"
+fi
